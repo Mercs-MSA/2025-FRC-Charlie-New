fe76f827f162a6fe0fa6e2bbf794e87ea0224698
JathinB <battepatijathin@gmail.com>
2025-02-05 20:49:16 -0600

    hopefully fix for MERCS Lighting

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -205,7 +205,6 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
     }
-    new CommandCandleSetAnimation(m_robotContainer.m_leds, AnimationTypes.Twinkle);
   }
 
   @Override
@@ -219,7 +218,6 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
-    new CommandCandleSetAnimation(m_robotContainer.m_leds, AnimationTypes.Twinkle);
   }
 
   @Override
