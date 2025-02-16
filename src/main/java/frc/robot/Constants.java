86d032b296470aa4319b8e07c309b5da54ff794d
Mercenaries Software Team <software@mckinneysteamacademy.org>
2025-02-16 16:59:23 -0600

    grayson is a nerd

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -178,8 +178,8 @@ public class Constants {
         public static final double linearMetersMaxVel = 3.0;
         public static final double linearMetersMaxAccel = 20.0;
         public static final HashMap<String, CommandToPos.Destination> positions = new HashMap<String, CommandToPos.Destination>() {{
-            put("reefA", new CommandToPos.Destination("reefA", new Pose2d(3.175, 4.181, new Rotation2d(0))));
-            put("reefB", new CommandToPos.Destination("reefB", new Pose2d(3.175, 3.882, new Rotation2d(0))));//
+            put("reefA", new CommandToPos.Destination("reefA", new Pose2d(3.169, 4.247, new Rotation2d(0))));
+            put("reefB", new CommandToPos.Destination("reefB", new Pose2d(3.178, 3.892, new Rotation2d(0))));//
             put("reefC", new CommandToPos.Destination("reefC", new Pose2d(3.679, 2.958, new Rotation2d(1.047))));
             put("reefD", new CommandToPos.Destination("reefD", new Pose2d(3.961, 2.801, new Rotation2d(1.047))));
             put("reefE", new CommandToPos.Destination("reefE", new Pose2d(5.00, 2.789, new Rotation2d(2.0944))));
