8dd930f35043c9a578f98f409d4ceaf15dadf926
Mercenaries Software Team <software@mckinneysteamacademy.org>
2025-03-03 22:22:06 -0600

    Software mentors always get the last commit.

--- src/main/java/frc/robot/commands/DriveToPosCommands/CommandToPos.java
+++ src/main/java/frc/robot/commands/DriveToPosCommands/CommandToPos.java
@@ -23,11 +23,11 @@ public class CommandToPos extends Command {
     new ProfiledPIDController(6, 1.0, 0, new TrapezoidProfile.Constraints(Math.PI, Math.PI));
     // new PIDController(2, 0, 0);
   private final ProfiledPIDController xVelController =
-    new ProfiledPIDController(2.5, 0, 0, new TrapezoidProfile.Constraints(Constants.DriveToPoseConstants.linearMetersMaxVel, Constants.DriveToPoseConstants.linearMetersMaxAccel));
+    new ProfiledPIDController(3.5, 0, 0, new TrapezoidProfile.Constraints(Constants.DriveToPoseConstants.linearMetersMaxVel, Constants.DriveToPoseConstants.linearMetersMaxAccel));
     // new PIDController(2.5, 0, 0);
   private final ProfiledPIDController yVelController =
     // new PIDController(2.5, 0, 0);
-    new ProfiledPIDController(2.5, 0, 0, new TrapezoidProfile.Constraints(Constants.DriveToPoseConstants.linearMetersMaxVel, Constants.DriveToPoseConstants.linearMetersMaxAccel));
+    new ProfiledPIDController(3.5, 0, 0, new TrapezoidProfile.Constraints(Constants.DriveToPoseConstants.linearMetersMaxVel, Constants.DriveToPoseConstants.linearMetersMaxAccel));
 
   public static class Destination {
     public Pose2d destPose;
